dba12bac6c26ab1605d5d3739f1d43d56b4cf874
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-09-14 23:43:11 -0300

    Fixing android navigation color problem - now bright colour

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { View, StatusBar } from "react-native";
 import Stack from "./src/navigation/Stack";
+import changeNavigationBarColor from "react-native-navigation-bar-color";
 
 console.disableYellowBox = true;
 
-const App = () => {
-  return(
-    <Stack />
-  )
+
+export default class App extends React.Component {
+  componentDidMount() {
+    changeNavigationBarColor("#f1f1f1")
+  }
+
+  render() {
+    return(
+      <Stack />
+    )
+  }
 }
-export default App;
